e18816aeee4e114fbb389ac8ea77779f0e0435fe
Josh <joshuaisaact@gmail.com>
2024-09-27 09:13:03 +0100

    fix: removed globe state from useReducer - this should be handled locally

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,8 +4,6 @@ export const initialState: State = {
   destinations: [] as Destination[],
   trips: [] as Triptype[],
   isLoading: false,
-  globeInstance: null,
-  points: [],
 };
 
 export function reducer(state: typeof initialState = initialState, action: Action) {
@@ -39,16 +37,6 @@ export function reducer(state: typeof initialState = initialState, action: Actio
       return {
         ...state,
         destinations: [...state.destinations, action.payload],
-      };
-    case 'SET_GLOBE_INSTANCE': // Handling globe instance
-      return {
-        ...state,
-        globeInstance: action.payload,
-      };
-    case 'SET_POINTS': // Handling globe points
-      return {
-        ...state,
-        points: action.payload,
       };
     default:
       return state;
