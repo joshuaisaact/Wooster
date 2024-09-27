2add485abd2694d66cf0b3a5a042df179989535a
Josh <joshuaisaact@gmail.com>
2024-09-27 08:52:58 +0100

    fix: removed props from applayout (not needed any more)

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import { useAppContext } from '@/hooks/useAppContext';
+import { useEffect, useReducer } from 'react';
+import { initialState, reducer } from '@/store/reducer';
 import Sidebar from '../components/Sidebar';
 import Trip from './Trip';
 import Trips from './TripsList';
@@ -10,27 +11,43 @@ import DestinationFullList from '@/components/DestinationFullList';
 import DestinationList from '@/components/DestinationList';
 import DestinationSummary from './DestinationSummary';
 import GlobeComponent from './GlobeComponent';
-import { AppProvider } from '@/context/AppContext';
-import type { Destination, Trip as Triptype } from '@/types/types';
 
 function AppLayout() {
-  const { state, dispatch } = useAppContext();
+  const BASE_URL = 'http://localhost:4000';
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  function addNewTrip(newTrip: Triptype) {
-    dispatch({ type: 'ADD_TRIP', payload: newTrip });
+  async function fetchTrips() {
+    try {
+      dispatch({ type: 'SET_LOADING', payload: true });
+      const res = await fetch(`${BASE_URL}/tripsdb`);
+      const data = await res.json();
+      dispatch({ type: 'SET_TRIPS', payload: data });
+    } catch (error) {
+      console.error('There was an error loading trips:', error);
+    } finally {
+      dispatch({ type: 'SET_LOADING', payload: false });
+    }
   }
 
-  function handleAddNewDestination(newDestination: Destination) {
-    dispatch({ type: 'ADD_DESTINATION', payload: newDestination });
+  async function fetchDestinations() {
+    try {
+      dispatch({ type: 'SET_LOADING', payload: true });
+      const res = await fetch(`${BASE_URL}/destinations`);
+      const data = await res.json();
+      dispatch({ type: 'SET_DESTINATIONS', payload: data });
+    } catch (error) {
+      console.error('There was an error loading destinations:', error);
+    } finally {
+      dispatch({ type: 'SET_LOADING', payload: false });
+    }
   }
 
-  function handleRemoveDestination(deletedDestinationId: number) {
-    dispatch({ type: 'REMOVE_DESTINATION', payload: deletedDestinationId });
-  }
+  console.log('Fetched trips and destinations');
 
-  function handleDeleteTrip(deletedTripId: string) {
-    dispatch({ type: 'REMOVE_TRIP', payload: deletedTripId });
-  }
+  useEffect(() => {
+    fetchTrips();
+    fetchDestinations();
+  }, [dispatch]);
 
   return (
     <div className="flex min-h-screen">
@@ -40,54 +57,21 @@ function AppLayout() {
       <div className="ml-64 flex-1 flex-grow flex-col items-center p-4">
         <Routes>
           <Route path="home" element={<Dashboard />} />
-          <Route
-            path="trips"
-            element={
-              <Trips
-                trips={state.trips}
-                destinations={state.destinations}
-                isLoading={state.isLoading}
-                addNewTrip={addNewTrip}
-                dispatch={dispatch}
-              />
-            }
-          />
-          <Route
-            path="trips/:tripId"
-            element={<Trip trips={state.trips} destinations={state.destinations} />}
-          >
+          <Route path="trips" element={<Trips />} />
+          <Route path="trips/:tripId" element={<Trip />}>
             <Route path="summary/:destinationId" element={<DestinationSummary />} />
           </Route>
           <Route
             path="explore"
             element={
               <Explore>
-                <DestinationList
-                  destinations={state.destinations}
-                  handleAddNewDestination={handleAddNewDestination}
-                />
+                <DestinationList />
               </Explore>
             }
           />
-          <Route
-            path="destinations/:destinationId"
-            element={
-              <DestinationSummary
-                state={state}
-                onDeleteDestination={handleRemoveDestination}
-                onDeleteTrip={handleDeleteTrip}
-                addNewTrip={addNewTrip}
-                dispatch={dispatch}
-              />
-            }
-          />
+          <Route path="destinations/:destinationId" element={<DestinationSummary />} />
           <Route path="globe" element={<GlobeComponent />} />
-          <Route
-            path="destination-list"
-            element={
-              <DestinationFullList destinations={state.destinations} isLoading={state.isLoading} />
-            }
-          />
+          <Route path="destination-list" element={<DestinationFullList />} />
           <Route path="settings" element={<Profile />} />
         </Routes>
       </div>
