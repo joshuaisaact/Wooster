6f343a31105846db154aed7062a342aaf457b7fe
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-10-05 18:19:11 +0100

    feat: menu icons

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -1,27 +1,49 @@
 import SidebarNavLink from './SidebarNavLink';
+import {
+  HiOutlineCog6Tooth,
+  HiOutlineGlobeEuropeAfrica,
+  HiOutlineHome,
+  HiOutlineMap,
+} from 'react-icons/hi2';
+import { IoAirplaneOutline } from 'react-icons/io5';
 
 function SidebarNav() {
   return (
     <nav className="flex w-64 justify-center p-6">
       <ul className="space-y-6">
         <li>
-          <SidebarNavLink to="/home" label="Dashboard" />
+          <SidebarNavLink to="/home">
+            <HiOutlineHome className="h-6 w-6" />
+            <span>Home</span>
+          </SidebarNavLink>
         </li>
         <li>
-          <SidebarNavLink to="/trips" label="Trips" />
+          <SidebarNavLink to="/trips">
+            <IoAirplaneOutline className="h-6 w-6" />
+            Trips
+          </SidebarNavLink>
         </li>
         <li>
-          <SidebarNavLink to="/destination-list" label="Destinations" />
+          <SidebarNavLink to="/destination-list">
+            <HiOutlineMap className="h-6 w-6" />
+            <span>Destinations</span>
+          </SidebarNavLink>
         </li>
         <li>
-          <SidebarNavLink to="/explore" label="Explore" />
+          <SidebarNavLink to="/explore">
+            <HiOutlineGlobeEuropeAfrica className="h-6 w-6" />
+            <span>Explore</span>
+          </SidebarNavLink>
         </li>
         {/* <li>
           <SidebarNavLink to="/globe" label="Globe" />
         </li> */}
 
         <li>
-          <SidebarNavLink to="/settings" label="Settings" />
+          <SidebarNavLink to="/settings">
+            <HiOutlineCog6Tooth className="h-6 w-6" />
+            <span>Settings</span>
+          </SidebarNavLink>
         </li>
       </ul>
     </nav>
--- src/components/SidebarNavLink.tsx
+++ src/components/SidebarNavLink.tsx
@@ -1,21 +1,22 @@
+import { ReactNode } from 'react';
 import { NavLink } from 'react-router-dom';
 
 interface SidebarNavLinkProps {
   to: string;
-  label: string;
+  children: ReactNode;
 }
 
-function SidebarNavLink({ to, label }: SidebarNavLinkProps) {
+function SidebarNavLink({ to, children }: SidebarNavLinkProps) {
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         isActive
-          ? 'flex items-center justify-center rounded-lg bg-gray-700 px-4 py-2 text-lg font-semibold text-white'
-          : 'flex items-center justify-center rounded-lg px-4 py-2 text-lg font-semibold text-white transition-colors duration-200 hover:bg-accent hover:text-white'
+          ? 'gap-5font-semibold flex items-center gap-3 rounded-lg bg-gray-700 px-4 py-2 text-lg text-white transition-all'
+          : 'flex items-center gap-3 rounded-lg px-4 py-2 text-lg font-semibold text-white transition-all transition-colors duration-200 hover:bg-accent hover:text-white'
       }
     >
-      {label}
+      {children}
     </NavLink>
   );
 }
