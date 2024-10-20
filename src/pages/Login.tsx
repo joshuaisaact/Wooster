bc38480d16cbcc509084d5476f7f4d166666ae0b
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-10-20 07:41:14 +0100

    fix: typesafety

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,7 +23,7 @@ function Login() {
     [isAuthenticated, navigate],
   );
 
-  function handleSubmit(e) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     if (email && password) login(email, password);
