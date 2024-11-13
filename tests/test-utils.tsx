5cb36adbe22a225b5160820fe2b42d8f7d817bd0
Joshua Tuddenham <joshuaisaact@gmail.com>
2024-11-13 14:23:01 +0000

    chore: fixed unused import for build process

--- tests/test-utils.tsx
+++ tests/test-utils.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode } from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { FormProvider, useForm } from 'react-hook-form';
